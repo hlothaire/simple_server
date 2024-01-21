3775cf4014e8e0d443d5b39d14a7a3dbc7fc49ef
hlothaire <lothaire.holcvart78@gmail.com>
2024-01-21 14:33:37 +0100

    added methods

--- http_tcpServer.h
+++ http_tcpServer.h
@@ -1,6 +1,12 @@
 #ifndef INCLUDED_HTTP_TCPSERVER
 #define INCLUDED_HTTP_TCPSERVER
 
+#include <arpa/inet.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string>
+#include <sys/socket.h>
+
 namespace http {
 
 class TcpServer {
@@ -9,6 +15,14 @@ public:
   ~TcpServer();
 
 private:
+  std::string _ip_address;
+  int _port;
+  int _scoket;
+  int _new_socket;
+  long _incommingMessage;
+  struct sockaddr_in _socketAddress;
+  unsigned int _socketAddress_len;
+  std::string _serverMessage;
 };
 
 } // namespace http

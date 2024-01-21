31b691214689ef3a70cfbb7f264ff77e942237f8
hlothaire <lothaire.holcvart78@gmail.com>
2024-01-21 11:15:33 +0100

    added class and namesapce

---
+++ http_tcpServer.h
@@ -0,0 +1,16 @@
+#ifndef INCLUDED_HTTP_TCPSERVER
+#define INCLUDED_HTTP_TCPSERVER
+
+namespace http {
+
+class TcpServer {
+public:
+  TcpServer();
+  ~TcpServer();
+
+private:
+};
+
+} // namespace http
+
+#endif // !INCLUDED_HTTP_TCPSERVER
